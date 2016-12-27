eed89a2677a580b56a004f976b4f688944ba70da
Mikael <miketekle14@gmail.com>
2016-12-27 11:17:03 -0800

    added reusable components to project and rendered header

---
+++ src/components/common/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { height: 2, width: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export { Header };
